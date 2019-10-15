d486ade6f949c8b5b4f4c8c9abd73e4db60b8d41
xavierLiu <lxr_orz@126.com>
2019-10-15 17:00:47 +0800

    The problem shows when bypass exists. A global map of network structure is needed.

--- xavier_lib.py
+++ xavier_lib.py
@@ -4,32 +4,79 @@ import numpy as np
 
 class InfoStruct(object):
 
-    def __init__(self, module, pre_f_cls, f_cls, b_cls):
+    def __init__(self, module, f_cls, b_cls):
+
+        # init
         self.module = module
-        self.pre_f_cls = pre_f_cls
         self.f_cls = f_cls
         self.b_cls = b_cls
 
-        self.stacked_ops = None
+        # forward statistic
+        self.forward_mean = None
+        self.forward_cov = None
+        self.channel_num = None
+        self.zero_variance_masked_zero = None
+        self.alpha = None  # variance after de-correlation
+        self.stack_op_for_weight = None
+
+        # backward statistic
+        self.grad_mean = None
+        self.grad_cov = None
+        self.adjust_matrix = None
+
+        # score
         self.score = None
-        self.arg_sort = None
+        self.sorted_index = None
+
+        # parameters form model
+        self.weight = None
+
+        # assigned before pruning
+        self.bn_module = None
+
+    def compute_statistic_and_fetch_weight(self):
+        # compute forward covariance
+        self.forward_mean = self.f_cls.sum_mean / self.f_cls.counter
+        self.forward_cov = (self.f_cls.sum_covariance / self.f_cls.counter) - \
+            torch.mm(self.forward_mean.view(-1, 1), self.forward_mean.view(1, -1))
+
+        self.channel_num = list(self.forward_cov.shape)[0]
+
         # equal 0 where variance of an activate is 0
-        self.zero_variance_mask = None
+        self.zero_variance_masked_zero = torch.sign(torch.diag(self.forward_cov))
 
-    def record(self, ops, score, zero_variance_mask):
-        self.stacked_ops = ops
-        self.score = score
-        self.arg_sort = torch.argsort(score)
-        self.zero_variance_mask = zero_variance_mask
+        # where 0 var compensate 1
+        repaired_forward_cov = self.forward_cov + \
+            torch.diag(torch.nn.Parameter(torch.ones(self.channel_num, dtype=torch.double)).cuda() - \
+                       self.zero_variance_masked_zero)
 
-    def last_value(self):
-        for i in range(list(self.score.shape)[0]):
-            index = int(self.arg_sort[i])
-            if int(self.zero_variance_mask[index]) != 0:
-                return index, float(self.score[index])
+        f_cov_inverse = repaired_forward_cov.inverse().to(torch.float)
+        repaired_alpha = torch.reciprocal(torch.diag(f_cov_inverse))
+        self.alpha = repaired_alpha *  self.zero_variance_masked_zero.to(torch.float)
 
-    def prune_a_channel(self, index):
-        pass
+        self.stack_op_for_weight = (f_cov_inverse.t() * repaired_alpha.view(1, -1)).t()
+
+        self.weight = self.module.weight.detach()
+
+        self.grad_mean = self.b_cls.sum_mean / self.b_cls.counter
+        self.grad_cov = (self.b_cls.sum_covariance / self.b_cls.counter) - \
+            torch.mm(self.grad_mean.view(-1, 1), self.grad_mean.view(1, -1))
+        eig_value, eig_vec = torch.eig(self.grad_cov, eigenvectors=True)
+
+        self.adjust_matrix = torch.mm(torch.diag(torch.sqrt(eig_value[:, 0])), eig_vec.t()).to(torch.float)
+        # print('M: ', adjust_matrix.shape)
+
+        adjusted_weight = torch.norm(torch.mm(self.adjust_matrix, torch.squeeze(self.weight)), dim=0)
+        self.score = adjusted_weight * self.alpha
+        self.sorted_index = torch.argsort(self.score)
+        print(self.score)
+
+    def clear_zero_variance(self):
+        if self.bn_module is None:
+            print('Modify biase in', self.module)
+
+        else:
+            pass
 
 
 def compute_statistic_and_update(samples, sum_mean, sum_covar, counter) -> None:
@@ -93,30 +140,6 @@ class BackwardStatisticHook(object):
             compute_statistic_and_update(samples, self.sum_mean, self.sum_covariance, self.counter)
 
 
-class PreForwardHook(object):
-
-    def __init__(self, name, dim=4):
-        self.name = name
-        self.dim=dim
-        self.mask = None
-        self.base = None
-
-    def __call__(self, module, inputs):
-        channel_num = list(inputs[0].shape)[1]
-        if self.mask is None:
-            self.mask = torch.nn.Parameter(torch.ones(channel_num), requires_grad=False).cuda()
-            self.base = torch.nn.Parameter(torch.zeros(channel_num), requires_grad=False).cuda()
-        if self.dim == 4:
-            modified = torch.mul(inputs[0].permute([0, 2, 3, 1]), self.mask) + self.base
-            return tuple(modified.permute([0, 3, 1, 2]), )
-        elif self.dim == 2:
-            return tuple(torch.mul(inputs, self.mask) + self.base, )
-
-    def update_mask_base(self, new_mask, new_base):
-        self.mask.data = new_mask
-        self.base.data = new_base
-
-
 class StatisticManager(object):
 
     def __init__(self):
@@ -127,27 +150,37 @@ class StatisticManager(object):
     def __call__(self, model):
 
         for name, sub_module in model.named_modules():
+
             if isinstance(sub_module, torch.nn.Conv2d):
                 if sub_module.kernel_size[0] == 1:
-                    pre_hook_cls = PreForwardHook(name)
                     hook_cls = ForwardStatisticHook(name)
                     back_hook_cls = BackwardStatisticHook(name)
-                    sub_module.register_forward_pre_hook(pre_hook_cls)
                     sub_module.register_forward_hook(hook_cls)
                     sub_module.register_backward_hook(back_hook_cls)
-                    self.name_to_statistic[name] = InfoStruct(sub_module, pre_hook_cls, hook_cls, back_hook_cls)
+                    self.name_to_statistic[name] = InfoStruct(sub_module, hook_cls, back_hook_cls)
                     print('conv', name)
+
             elif isinstance(sub_module, torch.nn.Linear):
-                pre_hook_cls = PreForwardHook(name, dim=2)
                 hook_cls = ForwardStatisticHook(name, dim=2)
                 back_hook_cls = BackwardStatisticHook(name, dim=2)
                 sub_module.register_forward_hook(hook_cls)
                 sub_module.register_backward_hook(back_hook_cls)
-                self.name_to_statistic[name] = InfoStruct(sub_module, pre_hook_cls, hook_cls, back_hook_cls)
+                self.name_to_statistic[name] = InfoStruct(sub_module, hook_cls, back_hook_cls)
                 print('conv', name)
+
             elif isinstance(sub_module, torch.nn.BatchNorm1d) or isinstance(sub_module, torch.nn.BatchNorm2d):
                 self.bn_name[name] = sub_module
                 print('bn', name)
+
+    def computer_score(self):
+
+        with torch.no_grad():
+
+            for name in self.name_to_statistic:
+
+                info = self.name_to_statistic[name]
+
+                info.compute_statistic_and_fetch_weight()
 
     def visualize(self):
 
@@ -175,49 +208,3 @@ class StatisticManager(object):
             if i > 150:
                 break
         plt.show()
-
-    def computer_score(self):
-
-        with torch.no_grad():
-
-            for name in self.name_to_statistic:
-
-                info = self.name_to_statistic[name]
-
-                # compute forward covariance
-                forward_mean = info.f_cls.sum_mean / info.f_cls.counter
-                forward_cov = (info.f_cls.sum_covariance / info.f_cls.counter) - \
-                    torch.mm(forward_mean.view(-1, 1), forward_mean.view(1, -1))
-                # print('f: ', forward_cov.shape, torch.matrix_rank(forward_cov))
-
-                channel_num = list(forward_cov.shape)[0]
-
-                # equal 0 where variance of an activate is 0
-                zero_variance_mask = torch.sign(torch.diag(forward_cov))
-
-                forward_cov += torch.diag(torch.nn.Parameter(torch.ones(channel_num, dtype=torch.double)).cuda()-\
-                                          zero_variance_mask)
-
-                f_cov_inverse = forward_cov.inverse().to(torch.float)
-                alpha = torch.reciprocal(torch.diag(f_cov_inverse))
-
-                stack_op_for_weight = (f_cov_inverse.t() * alpha.view(1, -1)).t()
-
-                # print(alpha)
-
-                weight = info.module.weight.detach()
-                # print('module: ', weight.shape)
-
-                grad_mean = info.b_cls.sum_mean / info.b_cls.counter
-                grad_cov = (info.b_cls.sum_covariance / info.b_cls.counter) - \
-                    torch.mm(grad_mean.view(-1, 1), grad_mean.view(1, -1))
-                eig_value, eig_vec = torch.eig(grad_cov, eigenvectors=True)
-
-                adjust_matrix = torch.mm(torch.diag(torch.sqrt(eig_value[:, 0])), eig_vec.t()).to(torch.float)
-                # print('M: ', adjust_matrix.shape)
-
-                adjusted_weight = torch.abs(torch.mm(adjust_matrix, torch.squeeze(weight)))
-                score = torch.mm(adjusted_weight, alpha.view([-1, 1])).view([-1])
-                # print
-
-                info.record(stack_op_for_weight, score, zero_variance_mask)
